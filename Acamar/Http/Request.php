3636f65bdde4ea34087dba05a9172dc4c3b73541
brian978 <andrei.dincescu1@gmail.com>
2014-11-19 22:40:47 +0200

    Fixed bug with the request object

--- Acamar/Http/Request.php
+++ Acamar/Http/Request.php
@@ -61,6 +61,76 @@ class Request
      */
     public function getRequestUri()
     {
+        if (!isset($this->server['PATH_INFO'])) {
+            $this->detectPathInfo();
+        }
+
         return $this->server['PATH_INFO'];
+    }
+
+    /**
+     * Returns the query string in array format
+     *
+     * @return array
+     */
+    public function getQuery()
+    {
+        if (empty($this->server['QUERY_STRING']) && strpos($this->server['REQUEST_URI'], '?')) {
+            $this->detectQueryString();
+        }
+
+        // TODO: optimize this
+        parse_str($this->server['QUERY_STRING'], $output);
+
+        return $output;
+    }
+
+    /**
+     * Detects the request path
+     *
+     * @return $this
+     * @throws \RuntimeException
+     */
+    protected function detectPathInfo()
+    {
+        if (!isset($this->server['REQUEST_URI'])) {
+            throw new \RuntimeException('Cannot detect the path info due to lack of information');
+        }
+
+        $requestUri = $this->server['REQUEST_URI'];
+        $scriptName = str_replace('/index.php', '', $this->server['SCRIPT_NAME']);
+
+        if (empty($this->server['QUERY_STRING']) && strpos($this->server['REQUEST_URI'], '?')) {
+            $this->detectQueryString();
+        }
+
+        // Removing the query string from the request URI
+        $requestUri = str_replace('?' . $this->server['QUERY_STRING'], '', $requestUri);
+        $requestUri = preg_replace('#.*' . $scriptName . '#', '', $requestUri);
+
+        // Updating the PATH_INFO with the proper information (ensuring right slash)
+        $this->server['PATH_INFO'] = '/' . rtrim($requestUri, '/');
+
+        return $this;
+    }
+
+    /**
+     * Detects the query string
+     *
+     * @return $this
+     */
+    protected function detectQueryString()
+    {
+        $requestUri = $this->server['REQUEST_URI'];
+
+        // If "nginx" is used then it may be configured wrong and we may not have the `QUERY_STRING`
+        $queryString = $this->server['QUERY_STRING'];
+        if (($argsPos = strpos($requestUri, '?')) !== false && empty($queryString)) {
+            $queryString = substr($requestUri, $argsPos + 1);
+        }
+
+        $this->server['QUERY_STRING'] = $queryString;
+
+        return $this;
     }
 }
