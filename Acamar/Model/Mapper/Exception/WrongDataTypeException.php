3bc359510c1968d19eca1a4223ca43c70f58e2d8
brian978 <andrei.dincescu1@gmail.com>
2014-11-26 23:23:46 +0200

    Updated comment

--- Acamar/Model/Mapper/Exception/WrongDataTypeException.php
+++ Acamar/Model/Mapper/Exception/WrongDataTypeException.php
@@ -2,13 +2,20 @@
 /**
  * Acamar-Framework
  *
- * @author Andrei Dincescu <andrei.dincescu@ubisoft.com
+ * @link      https://github.com/brian978/Acamar-Framework
+ * @copyright Copyright (c) 2013
+ * @license   Creative Commons Attribution-ShareAlike 3.0
  */
 
 namespace Acamar\Model\Mapper\Exception;
 
 use InvalidArgumentException;
 
+/**
+ * Class WrongDataTypeException
+ *
+ * @package Acamar\Model\Mapper\Exception
+ */
 class WrongDataTypeException extends InvalidArgumentException
 {
 
