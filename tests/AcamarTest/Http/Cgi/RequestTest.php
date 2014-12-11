ddd4b92f215d15bc0aae884b7f7e5f29d7eb885c
brian978 <andrei.dincescu1@gmail.com>
2014-12-11 13:51:01 +0200

    Added failing tests for the CGI Request class

---
+++ tests/AcamarTest/Http/Cgi/RequestTest.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Acamar-Framework
+ *
+ * @link https://github.com/brian978/Acamar-Framework
+ * @copyright Copyright (c) 2014
+ * @license https://github.com/brian978/Acamar-Framework/blob/master/LICENSE.txt New BSD License
+ */
+
+namespace AcamarTest\Http\Cgi;
+
+use Acamar\Http\Cgi\Request;
+
+/**
+ * Class RequestTest
+ *
+ * @package AcamarTest\Http\Cgi
+ */
+class RequestTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * this is used to simulate the $_SERVER super global
+     *
+     * @return array
+     */
+    public function serverDataProvider()
+    {
+        $server = require realpath(__DIR__ . '/_files/server.php');
+
+        return array(
+            array($server)
+        );
+    }
+
+    /**
+     * @param array $server
+     * @dataProvider serverDataProvider
+     * @covers Request::getMethod
+     */
+    public function testCanGetRequestMethod(array $server)
+    {
+        $request = new Request($server);
+
+        $this->assertEquals('GET', $request->getMethod());
+    }
+
+    /**
+     * @param array $server
+     * @dataProvider serverDataProvider
+     * @covers Request::setPathInfo
+     */
+    public function testCanGetUri(array $server)
+    {
+        $request = new Request($server);
+
+        $this->assertEquals('/controller/action', $request->getUri());
+    }
+
+    /**
+     * @param array $server
+     * @dataProvider serverDataProvider
+     * @covers Request::setPathInfo
+     */
+    public function testCanGetWithRootBaseFolder(array $server)
+    {
+        $server['SCRIPT_NAME'] = '/index.php';
+        $server['REQUEST_URI'] = '/controller/action';
+
+        $request = new Request($server);
+
+        $this->assertEquals('/controller/action', $request->getUri());
+    }
+}
--- tests/AcamarTest/Http/Cgi/_files/server.php
+++ tests/AcamarTest/Http/Cgi/_files/server.php
@@ -2,8 +2,8 @@
 
 return array(
     'REQUEST_METHOD' => 'GET',
-    'SCRIPT_NAME' => '/index.php',
-    'REQUEST_URI' => '/controller/action',
+    'SCRIPT_NAME' => '/folder/BasePath/index.php',
+    'REQUEST_URI' => '/folder/BasePath/controller/action',
     'QUERY_STRING' => 'test=1',
     'REMOTE_ADDR' => '127.0.0.1'
 );
