a931868329f2843c3cf4d6fbec5b8ac9129e6d77
brian978 <andrei.dincescu1@gmail.com>
2014-12-11 12:58:24 +0200

    Added test files for the CGI Request object

---
+++ tests/AcamarTest/Http/Cgi/_files/server.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+    'REQUEST_METHOD' => 'GET',
+    'SCRIPT_NAME' => '/index.php',
+    'REQUEST_URI' => '/controller/action',
+    'QUERY_STRING' => 'test=1',
+    'REMOTE_ADDR' => '127.0.0.1'
+);
