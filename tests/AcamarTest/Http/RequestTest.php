3c32ebb1ad3e6bd2718e803ea4137fb26afe257a
brian978 <andrei.dincescu1@gmail.com>
2014-12-11 12:41:17 +0200

    Added tests the for base Acamar\Http\Request class

---
+++ tests/AcamarTest/Http/RequestTest.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Acamar-Framework
+ *
+ * @link https://github.com/brian978/Acamar-Framework
+ * @copyright Copyright (c) 2014
+ * @license https://github.com/brian978/Acamar-Framework/blob/master/LICENSE.txt New BSD License
+ */
+
+namespace AcamarTest\Http;
+
+use Acamar\Http\Request;
+
+/**
+ * Class RequestTest
+ *
+ * @package AcamarTest\Http
+ */
+class RequestTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers Request::getHeaders
+     */
+    public function testReturnsEmptyHeadersObject()
+    {
+        $this->assertInstanceOf('\Acamar\Http\Headers', (new Request())->getHeaders());
+    }
+
+    /**
+     * @return array
+     */
+    public function requestQuerySetterDataProvider()
+    {
+        return array(
+            array('test', new \stdClass(), null),
+            array('test2', 'testString', 'testString'),
+            array('test3', 12345, '12345')
+        );
+    }
+
+    /**
+     * @dataProvider requestQuerySetterDataProvider
+     * @covers Request::setQuery
+     */
+    public function testQuerySetterAcceptsOnlyStringOrNumber($name, $value, $assertValue)
+    {
+        $request = new Request();
+        $request->setQuery($name, $value);
+
+        $this->assertEquals($assertValue, $request->getQuery($name));
+    }
+
+    /**
+     * @return array
+     */
+    public function requestPostSetterDataProvider()
+    {
+        return array(
+            array('test', new \stdClass(), null),
+            array('test2', 'testString', 'testString'),
+            array('test3', 12345, '12345')
+        );
+    }
+
+    /**
+     * @dataProvider requestQuerySetterDataProvider
+     * @covers Request::setQuery
+     */
+    public function testPostSetterAcceptsOnlyStringOrNumber($name, $value, $assertValue)
+    {
+        $request = new Request();
+        $request->setPost($name, $value);
+
+        $this->assertEquals($assertValue, $request->getPost($name));
+    }
+}
