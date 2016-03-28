f9faa25a0336bece49cfcc511f7f7f7bf8e62700
brian978 <andrei.dincescu1@gmail.com>
2016-03-28 09:21:13 +0300

    Added php doc

--- tests/AcamarTest/Http/ResponseTest.php
+++ tests/AcamarTest/Http/ResponseTest.php
@@ -54,6 +54,7 @@ class ResponseTest extends \PHPUnit_Framework_TestCase
     /**
      * @covers       Acamar\Http\Response::fromString
      * @dataProvider responseProvider
+     * @param Response $response
      */
     public function testCreateResponseFromString(Response $response)
     {
@@ -64,6 +65,7 @@ class ResponseTest extends \PHPUnit_Framework_TestCase
     /**
      * @covers       Acamar\Http\Response::fromString
      * @dataProvider responseProvider
+     * @param Response $response
      */
     public function testCreateResponseFromStringWithHeaders(Response $response)
     {
@@ -73,6 +75,7 @@ class ResponseTest extends \PHPUnit_Framework_TestCase
     /**
      * @covers       Acamar\Http\Response::fromString
      * @dataProvider responseProvider
+     * @param Response $response
      */
     public function testCreateResponseFromStringWithStatusPhrase(Response $response)
     {
@@ -82,6 +85,7 @@ class ResponseTest extends \PHPUnit_Framework_TestCase
     /**
      * @covers       Acamar\Http\Response::fromString
      * @dataProvider responseProvider
+     * @param Response $response
      */
     public function testCreateResponseFromStringWithBody(Response $response)
     {
